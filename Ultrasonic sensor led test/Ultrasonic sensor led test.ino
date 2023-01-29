ed507ec41d1278fec7e00ab811f04726dcff8dbb
jannat28-349 <jucse2.349@gmail.com>
2023-01-30 01:51:42 +0600

    Added ultrasonic code for LED and Speed controlling

--- Ultrasonic sensor led test/Ultrasonic sensor led test.ino
+++ Ultrasonic sensor led test/Ultrasonic sensor led test.ino
@@ -1,87 +1,39 @@
-#define trigPin 4
-#define echoPin 3
-int l1 = 5;
-int l2 = 6;
-int r1 = 7;
-int r2 = 8;
-int ena = 9;
-int enb = 10;
+#define trigPin 13
+#define echoPin 12
+#define led 11
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(ena, OUTPUT);
-  pinMode(l1, OUTPUT);
-  pinMode(l2, OUTPUT);
-  pinMode(r1, OUTPUT);
-  pinMode(r2, OUTPUT);
-  pinMode(enb, OUTPUT);
-  Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+void setup()
+
+{ Serial.begin (9600);
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+pinMode(led, OUTPUT);
 }
 
-void goForward() {
-
-  digitalWrite(l1, HIGH);
-  digitalWrite(l2, LOW);
-  digitalWrite(r1, LOW);
-  digitalWrite(r2, HIGH);
-}
-void goStop() {
-  digitalWrite(l1, LOW);
-  digitalWrite(l2, LOW);
-  digitalWrite(r1, LOW);
-  digitalWrite(r2, LOW);
-}
-
-// int convertSpeedFromPotentiometer(int analogSpeed) {
-//   int speed;
-//   speed = int(0.249 * analogSpeed);
-//   return speed;
-// }
-
-void setSpeed(int speed) {
-
-  analogWrite(ena, speed);
-  analogWrite(enb, speed);
-}
-void loop() {
-  //int sensorValue = analogRead(A0);
-  //int speed = convertSpeedFromPotentiometer(sensorValue);
-  //setSpeed(speed);
-
+void loop()
+{ 
   long duration, distance;
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(100);
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
 
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(1000);
 
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration / 2) / 29.1;
-  if (distance > 300) {
-    setSpeed(180);
-    goForward();
-  }
-  else if(distance > 120 ){
-    setSpeed(100);
-    goForward();
-  }
-  else if (distance > 50) {
-    setSpeed(70);
-    goForward();
-  }
-  else
-  {
-    goStop();
-  }
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
 
+distance = (duration/2)/29.1;
 
+if (distance <20 )
+{ digitalWrite(led,HIGH);
+}
+else {
+digitalWrite(led,LOW);
+}
+Serial.print(distance);
 
-  // Serial.print(distance);
+Serial.println(" cm");
 
-  // Serial.println(" cm");
-  // delay(1000);
+delay(100);
+
 }
