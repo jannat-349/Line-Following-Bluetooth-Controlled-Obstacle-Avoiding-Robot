35aabd5bc842acacb27e4939e8f7440b04b8415d
jannat28-349 <jucse2.349@gmail.com>
2023-01-30 01:47:36 +0600

    Added potentiometer code with/without bluetooth

---
+++ Potentiometer/potentiometer_value_input/potentiometer_value_input.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  delay(1000);
+
+}
